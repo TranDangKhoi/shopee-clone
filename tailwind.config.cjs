6cf2ca07957386ad6be045c7e50c32e1c4e60b10
TranDangKhoi <baits16@gmail.com>
2023-02-19 16:12:30 +0700

    :heavy_plus_sign: Add new custom CSS attribute

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ee4d2d",
+      },
+      backgroundImage: {
+        "shopee-pattern": "url('https://cf.shopee.vn/file/5569eb9dc7e09e2dbed5315b8f2ea8ba')",
+      },
+    },
   },
   plugins: [],
 };
