2b749030ecb95a97cd4f83a00cf46a801131f151
TranDangKhoi <baits16@gmail.com>
2023-02-19 23:25:13 +0700

    :+1: Installed classNames

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  corePlugins: {
+    container: false,
+  },
   theme: {
     extend: {
       colors: {
@@ -11,5 +17,21 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        ".container": {
+          maxWidth: theme("columns.7xl"),
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingLeft: theme("spacing.4"),
+          paddingRight: theme("spacing.4"),
+          // Vì các trình duyệt phiên bản cũ (Chrome <2020, IE 9 11, ...v.v) chưa hỗ trợ kiểu styles này nên phải tạm comment để sử dụng kiểu trên
+          // marginInline: "auto",
+          // paddingInline: theme("spacing.4"),
+          // Nguồn: https://caniuse.com/?search=%20%20padding-inline và https://caniuse.com/?search=margin-inline
+        },
+      });
+    }),
+  ],
 };
