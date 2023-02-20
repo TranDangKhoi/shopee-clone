d462be874426e34ed8681d3b8adbb43c5d8faffa
TranDangKhoi <baits16@gmail.com>
2023-02-20 17:38:09 +0700

    :wink: Integrate tanstack query and created http instance

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
