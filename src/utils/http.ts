8ecad92fee65a4d0e432d94493bb6fdce8fc06cb
TranDangKhoi <baits16@gmail.com>
2023-02-20 18:52:48 +0700

    :100: Create a http instance for calling API

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -4,7 +4,7 @@ class Http {
   instance: AxiosInstance;
   constructor() {
     this.instance = axios.create({
-      baseURL: import.meta.env.BASE_URL,
+      baseURL: "https://api-ecom.duthanhduoc.com",
       timeout: 10000,
       headers: {
         "Content-Type": "application/json",
