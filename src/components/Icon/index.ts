527162e529777fe1270c6a837762811e000a8aef
TranDangKhoi <baits16@gmail.com>
2023-02-21 22:34:48 +0700

    :hearts: Create an index.ts for logos component

---
+++ src/components/Icon/index.ts
@@ -0,0 +1,5 @@
+import EarthIcon from "./EarthIcon";
+import ArrowDownIcon from "./ArrowDownIcon";
+import ShopeeLogoIcon from "./ShopeeLogoIcon";
+
+export { EarthIcon, ArrowDownIcon, ShopeeLogoIcon };
