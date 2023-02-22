6f23c52baa64307837511d3818380e817630eca8
TranDangKhoi <baits16@gmail.com>
2023-02-22 23:38:34 +0700

    :wrench: Discriminate routes for authenticated/unauthenticated users

--- src/routes/useRouteElements.tsx
+++ src/routes/useRouteElements.tsx
@@ -1,14 +1,27 @@
-import { useRoutes } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, Outlet, useRoutes } from "react-router-dom";
+import { AuthContext } from "src/contexts/auth.context";
 import AuthenticationLayout from "src/layouts/AuthenticationLayout";
 import MainLayout from "src/layouts/MainLayout";
 import Login from "src/pages/Login";
+import Profile from "src/pages/Profile";
 import Register from "src/pages/Register";
 import ProductList from "../pages/ProductList";
+function ProtectedRoute() {
+  const { isAuthenticated } = useContext(AuthContext);
+  return isAuthenticated ? <Outlet></Outlet> : <Navigate to="/login"></Navigate>;
+}
+
+function RejectedRoute() {
+  const { isAuthenticated } = useContext(AuthContext);
+  return !isAuthenticated ? <Outlet></Outlet> : <Navigate to="/"></Navigate>;
+}
 
 export default function useRoutesElement() {
   const routeElements = useRoutes([
     {
       path: "/",
+      index: true,
       element: (
         <MainLayout>
           <ProductList></ProductList>
@@ -16,20 +29,40 @@ export default function useRoutesElement() {
       ),
     },
     {
-      path: "/login",
-      element: (
-        <AuthenticationLayout>
-          <Login></Login>
-        </AuthenticationLayout>
-      ),
+      path: "",
+      element: <ProtectedRoute></ProtectedRoute>,
+      children: [
+        {
+          path: "/profile",
+          element: (
+            <MainLayout>
+              <Profile></Profile>
+            </MainLayout>
+          ),
+        },
+      ],
     },
     {
-      path: "/register",
-      element: (
-        <AuthenticationLayout>
-          <Register></Register>
-        </AuthenticationLayout>
-      ),
+      path: "",
+      element: <RejectedRoute></RejectedRoute>,
+      children: [
+        {
+          path: "/login",
+          element: (
+            <AuthenticationLayout>
+              <Login></Login>
+            </AuthenticationLayout>
+          ),
+        },
+        {
+          path: "/register",
+          element: (
+            <AuthenticationLayout>
+              <Register></Register>
+            </AuthenticationLayout>
+          ),
+        },
+      ],
     },
   ]);
   return routeElements;
