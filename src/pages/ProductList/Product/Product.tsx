5fc5a3058feaae9e4ecb7b2e2fe850a826364a54
TranDangKhoi <baits16@gmail.com>
2023-02-24 16:48:15 +0700

    :+1: Calculate star width through product's rating

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ProductRating from "src/components/ProductRating";
 import { path } from "src/constants/path";
 import { ProductType } from "src/types/product.type";
 import { formatCurrency, formatNumberToSocialStyle } from "src/utils/formatNumber";
@@ -31,43 +32,7 @@ const Product = ({ product }: ProductProps) => {
             </div>
           </div>
           <div className="mt-3 flex items-center justify-end gap-x-2">
-            <div className="flex items-center">
-              <div className="relative">
-                <div
-                  className="absolute top-0 left-0 h-full overflow-hidden"
-                  style={{ width: "50%" }}
-                >
-                  <svg
-                    enableBackground="new 0 0 15 15"
-                    viewBox="0 0 15 15"
-                    x={0}
-                    y={0}
-                    className="h-3 w-3 fill-yellow-300 text-yellow-300"
-                  >
-                    <polygon
-                      points="7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeMiterlimit={10}
-                    />
-                  </svg>
-                </div>
-                <svg
-                  enableBackground="new 0 0 15 15"
-                  viewBox="0 0 15 15"
-                  x={0}
-                  y={0}
-                  className="h-3 w-3 fill-current text-gray-300"
-                >
-                  <polygon
-                    points="7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeMiterlimit={10}
-                  />
-                </svg>
-              </div>
-            </div>
+            <ProductRating rating={product.rating}></ProductRating>
             <div className="flex gap-x-1 text-sm">
               <span>{formatNumberToSocialStyle(product.sold)}</span>
               <span>Đã bán</span>
