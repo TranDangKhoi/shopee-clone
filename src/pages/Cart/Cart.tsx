e20c171fc79ba7d81fef34e7af9dde4e13ee48af
TranDangKhoi <baits16@gmail.com>
2023-04-15 17:10:37 +0700

    :100: Successfully handling product buy

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -69,6 +69,23 @@ const Cart = () => {
     });
   }, [purchasesInCart]);
   const isAllChecked = extendedPurchases.every((purchase) => purchase.checked === true);
+
+  const handlePurchase = () => {
+    if (checkedPurchases.length > 0) {
+      const body = checkedPurchases.map((checkedPurchase) => ({
+        product_id: checkedPurchase.product._id,
+        buy_count: checkedPurchase.buy_count,
+      }));
+      buyProductsMutation.mutate(body, {
+        onSuccess: (data) => {
+          toast.success(data.data.message);
+        },
+      });
+    } else {
+      toast.info("Vui lòng chọn sản phẩm muốn mua");
+    }
+  };
+
   const handleSelectProduct = (productIndex: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
     setExtendedPurchases(
       produce((draft) => {
@@ -140,123 +157,127 @@ const Cart = () => {
             </div>
           </div>
         </div>
-        <div className="my-3 rounded-sm bg-white shadow sm:p-5">
-          {extendedPurchases?.map((purchase, index) => (
-            <div
-              key={purchase._id}
-              className="mb-5 flex items-center justify-between rounded-sm border border-gray-200 bg-white py-5 px-4 text-center text-sm text-gray-500 first:mt-0 lg:grid lg:grid-cols-12"
-            >
-              <div className="flex items-center gap-x-3 lg:col-span-6">
-                <div className="flex items-center justify-center gap-x-3">
-                  <input
-                    type="checkbox"
-                    className="h-5 w-5 accent-primary"
-                    checked={purchase.checked}
-                    onChange={handleSelectProduct(index)}
-                  />
-                  <Link
-                    className="h-14 w-14 flex-shrink-0 sm:h-20 sm:w-20"
-                    to={`${path.home}${generateSlug({
-                      name: purchase.product.name,
-                      id: purchase.product._id,
-                    })}`}
-                  >
-                    <img
-                      alt={purchase.product.name}
-                      src={purchase.product.image}
-                      className="h-14 w-14 object-cover sm:h-20 sm:w-20"
+        {extendedPurchases.length > 0 && (
+          <div className="my-3 rounded-sm bg-white shadow sm:p-5">
+            {extendedPurchases.map((purchase, index) => (
+              <div
+                key={purchase._id}
+                className="mb-5 flex items-center justify-between rounded-sm border border-gray-200 bg-white py-5 px-4 text-center text-sm text-gray-500 first:mt-0 lg:grid lg:grid-cols-12"
+              >
+                <div className="flex items-center gap-x-3 lg:col-span-6">
+                  <div className="flex items-center justify-center gap-x-3">
+                    <input
+                      type="checkbox"
+                      className="h-5 w-5 accent-primary"
+                      checked={purchase.checked}
+                      onChange={handleSelectProduct(index)}
                     />
-                  </Link>
-                </div>
-                <div className="flex flex-col items-start gap-y-1 lg:flex-row lg:gap-y-0">
-                  <Link
-                    to={`${path.home}${generateSlug({
-                      name: purchase.product.name,
-                      id: purchase.product._id,
-                    })}`}
-                    className="text-left line-clamp-1 lg:line-clamp-2"
-                  >
-                    {purchase.product.name}
-                  </Link>
-
-                  <div className="flex items-center gap-x-3 lg:hidden">
-                    <span className="text-gray-300 line-through">
-                      ₫{formatCurrency(purchase.product.price_before_discount)}
-                    </span>
-                    <span className="hidden lg:block">₫{formatCurrency(purchase.product.price)}</span>
-                    <span className="block text-primary lg:hidden">₫{formatCurrency(purchase.product.price)}</span>
-                  </div>
-                  <div className="flex gap-x-3 lg:hidden">
-                    <QuantityController
-                      max={purchase.product.quantity}
-                      value={purchase.buy_count}
-                      containerClassName="flex items-center"
-                      onIncrease={(value) => handleQuantity(index, value, value <= purchase.product.quantity)}
-                      onDecrease={(value) => handleQuantity(index, value, value >= 1)}
-                      onType={handleTypeQuantity(index)}
-                      onFocusOutside={(value) =>
-                        handleQuantity(
-                          index,
-                          value,
-                          value >= 1 && value <= (purchasesInCart as PurchaseType[])[index].buy_count,
-                        )
-                      }
-                      disabled={purchase.disabled}
-                    />
-                    <button
-                      onClick={handleDeleteAPurchase(index)}
-                      className="bg-none text-primary"
+                    <Link
+                      className="h-14 w-14 flex-shrink-0 sm:h-20 sm:w-20"
+                      to={`${path.home}${generateSlug({
+                        name: purchase.product.name,
+                        id: purchase.product._id,
+                      })}`}
                     >
-                      Xóa khỏi giỏ
-                    </button>
+                      <img
+                        alt={purchase.product.name}
+                        src={purchase.product.image}
+                        className="h-14 w-14 object-cover sm:h-20 sm:w-20"
+                      />
+                    </Link>
                   </div>
-                </div>
-              </div>
-              <div className="col-span-6">
-                <div className="grid grid-cols-5 items-center">
-                  <div className="col-span-2">
-                    <div className="hidden items-center justify-center gap-x-3 lg:flex">
+                  <div className="flex flex-col items-start gap-y-1 lg:flex-row lg:gap-y-0">
+                    <Link
+                      to={`${path.home}${generateSlug({
+                        name: purchase.product.name,
+                        id: purchase.product._id,
+                      })}`}
+                      className="text-left line-clamp-1 lg:line-clamp-2"
+                    >
+                      {purchase.product.name}
+                    </Link>
+
+                    <div className="flex items-center gap-x-3 lg:hidden">
                       <span className="text-gray-300 line-through">
                         ₫{formatCurrency(purchase.product.price_before_discount)}
                       </span>
                       <span className="hidden lg:block">₫{formatCurrency(purchase.product.price)}</span>
                       <span className="block text-primary lg:hidden">₫{formatCurrency(purchase.product.price)}</span>
                     </div>
+                    <div className="flex gap-x-3 lg:hidden">
+                      <QuantityController
+                        max={purchase.product.quantity}
+                        value={purchase.buy_count}
+                        containerClassName="flex items-center"
+                        onIncrease={(value) => handleQuantity(index, value, value <= purchase.product.quantity)}
+                        onDecrease={(value) => handleQuantity(index, value, value >= 1)}
+                        onType={handleTypeQuantity(index)}
+                        onFocusOutside={(value) =>
+                          handleQuantity(
+                            index,
+                            value,
+                            value >= 1 && value <= (purchasesInCart as PurchaseType[])[index].buy_count,
+                          )
+                        }
+                        disabled={purchase.disabled}
+                      />
+                      <button
+                        onClick={handleDeleteAPurchase(index)}
+                        className="bg-none text-primary"
+                      >
+                        Xóa khỏi giỏ
+                      </button>
+                    </div>
                   </div>
-                  <div className="col-span-1 hidden lg:block">
-                    <QuantityController
-                      max={purchase.product.quantity}
-                      value={purchase.buy_count}
-                      containerClassName="flex items-center"
-                      onIncrease={(value) => handleQuantity(index, value, value <= purchase.product.quantity)}
-                      onDecrease={(value) => handleQuantity(index, value, value >= 1)}
-                      onType={handleTypeQuantity(index)}
-                      onFocusOutside={(value) =>
-                        handleQuantity(
-                          index,
-                          value,
-                          value >= 1 && value <= (purchasesInCart as PurchaseType[])[index].buy_count,
-                        )
-                      }
-                      disabled={purchase.disabled}
-                    />
-                  </div>
-                  <div className="col-span-1 hidden lg:block">
-                    <span className="text-primary">₫{formatCurrency(purchase.product.price * purchase.buy_count)}</span>
-                  </div>
-                  <div className="col-span-1 hidden lg:block">
-                    <button
-                      onClick={handleDeleteAPurchase(index)}
-                      className="bg-none text-black transition-all hover:text-primary"
-                    >
-                      Xóa
-                    </button>
+                </div>
+                <div className="col-span-6">
+                  <div className="grid grid-cols-5 items-center">
+                    <div className="col-span-2">
+                      <div className="hidden items-center justify-center gap-x-3 lg:flex">
+                        <span className="text-gray-300 line-through">
+                          ₫{formatCurrency(purchase.product.price_before_discount)}
+                        </span>
+                        <span className="hidden lg:block">₫{formatCurrency(purchase.product.price)}</span>
+                        <span className="block text-primary lg:hidden">₫{formatCurrency(purchase.product.price)}</span>
+                      </div>
+                    </div>
+                    <div className="col-span-1 hidden lg:block">
+                      <QuantityController
+                        max={purchase.product.quantity}
+                        value={purchase.buy_count}
+                        containerClassName="flex items-center"
+                        onIncrease={(value) => handleQuantity(index, value, value <= purchase.product.quantity)}
+                        onDecrease={(value) => handleQuantity(index, value, value >= 1)}
+                        onType={handleTypeQuantity(index)}
+                        onFocusOutside={(value) =>
+                          handleQuantity(
+                            index,
+                            value,
+                            value >= 1 && value <= (purchasesInCart as PurchaseType[])[index].buy_count,
+                          )
+                        }
+                        disabled={purchase.disabled}
+                      />
+                    </div>
+                    <div className="col-span-1 hidden lg:block">
+                      <span className="text-primary">
+                        ₫{formatCurrency(purchase.product.price * purchase.buy_count)}
+                      </span>
+                    </div>
+                    <div className="col-span-1 hidden lg:block">
+                      <button
+                        onClick={handleDeleteAPurchase(index)}
+                        className="bg-none text-black transition-all hover:text-primary"
+                      >
+                        Xóa
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
         <div className="sticky bottom-0 z-10 mt-8 flex flex-col rounded-sm border border-gray-100 bg-white p-5 shadow lg:flex-row lg:items-center">
           <div className="flex items-center">
             <div className="flex flex-shrink-0 items-center justify-center pr-3">
@@ -287,7 +308,11 @@ const Cart = () => {
                 <div className="ml-6 text-primary">₫{formatCurrency(totalSavedPrice)}</div>
               </div>
             </div>
-            <Button className="mt-5 flex h-10 w-52 items-center justify-center bg-red-500 text-sm uppercase text-white hover:bg-red-600 lg:ml-4 lg:mt-0">
+            <Button
+              onClick={handlePurchase}
+              isLoading={buyProductsMutation.isLoading}
+              className="mt-5 flex h-10 w-52 items-center justify-center bg-red-500 text-sm uppercase text-white hover:bg-red-600 lg:ml-4 lg:mt-0"
+            >
               Mua hàng
             </Button>
           </div>
