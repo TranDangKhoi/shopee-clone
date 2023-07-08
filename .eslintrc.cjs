89c48f2a3491c4e64181fce6b3fe63e841f2d6d8
TranDangKhoi <baits16@gmail.com>
2023-07-08 19:10:18 +0700

    :+1: Testing out storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,24 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
-
 module.exports = {
-  extends: [
-    // Chúng ta sẽ dùng các rule mặc định từ các plugin mà chúng ta đã cài.
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:jsx-a11y/recommended",
-    "plugin:@typescript-eslint/recommended",
-    // Disable các rule mà eslint xung đột với prettier.
-    // Để cái này ở dưới để nó override các rule phía trên!.
-    "eslint-config-prettier",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:import/recommended", "plugin:import/typescript", "plugin:jsx-a11y/recommended", "plugin:@typescript-eslint/recommended", "eslint-config-prettier", "prettier", "plugin:storybook/recommended"],
   plugins: ["prettier"],
   settings: {
     react: {
       // Nói eslint-plugin-react tự động biết version của React.
-      version: "detect",
+      version: "detect"
     },
     // Nói ESLint cách xử lý các import
     "import/resolver": {
       node: {
         // Fix a path bug causing the root directory is not the current project - ESLint bug
         paths: [path.resolve(__dirname, "")],
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
-    },
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
   },
   env: {
-    node: true,
+    node: true
   },
   rules: {
     // Tắt rule yêu cầu import React trong file jsx
@@ -40,20 +26,17 @@ module.exports = {
     // Cảnh báo khi thẻ <a target='_blank'> mà không có rel="noreferrer"
     "react/jsx-no-target-blank": "warn",
     // Tăng cường một số rule prettier (copy từ file .prettierrc qua)
-    "prettier/prettier": [
-      "warn",
-      {
-        arrowParens: "always",
-        semi: true,
-        trailingComma: "all",
-        tabWidth: 2,
-        endOfLine: "auto",
-        useTabs: false,
-        singleQuote: false,
-        printWidth: 120,
-        jsxSingleQuote: false,
-        singleAttributePerLine: true,
-      },
-    ],
-  },
+    "prettier/prettier": ["warn", {
+      arrowParens: "always",
+      semi: true,
+      trailingComma: "all",
+      tabWidth: 2,
+      endOfLine: "auto",
+      useTabs: false,
+      singleQuote: false,
+      printWidth: 120,
+      jsxSingleQuote: false,
+      singleAttributePerLine: true
+    }]
+  }
 };
---
+++ .storybook/main.ts
@@ -0,0 +1,29 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+import path from "path";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-addon-react-router-v6",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          src: path.resolve(__dirname, "../src"),
+        },
+      },
+    });
+  },
+};
+export default config;
